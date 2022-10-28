e7058b79a29cb35265bb3cfa1f96190d79defd8d
Takamichi Omori <merrytakamerry@gmail.com>
2022-10-28 17:03:55 +0900

    add ruby

---
+++ ruby/spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require 'simplecov'
+
+if ENV['CI'] == 'true'
+  require 'simplecov-lcov'
+
+  SimpleCov::Formatter::LcovFormatter.config do |config|
+    config.report_with_single_file = true
+    config.single_report_path = 'coverage/lcov.info'
+  end
+
+  SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+end
+
+SimpleCov.start do
+  enable_coverage :branch
+end
